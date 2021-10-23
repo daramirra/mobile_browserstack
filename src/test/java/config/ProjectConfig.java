6e5b9cf2b06c5393f96a666874a52f1828b8ccd7
darina <linuxlinux>
2021-10-24 02:00:06 +0700

    add mobile tests

--- src/test/java/config/ProjectConfig.java
+++ src/test/java/config/ProjectConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/local.properties",
+        "classpath:config/mobile.properties",
         "classpath:config/remote.properties"
 })
 
